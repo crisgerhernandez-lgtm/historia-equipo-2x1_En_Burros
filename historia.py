3180bd56ac644e2415dc240718df5971812d7875
Axel <axel.aviles1010@gamil.com>
2026-08-17 23:36:38 -0600

    Parte 3 de la historia

--- historia.py
+++ historia.py
@@ -1,3 +1,3 @@
 print("Parte 1: Juan salió de su casa muy temprano.")
 print("Parte 2: Y en ese momento se encontro con una nave alienigena, apunto de abducirlo xd")
-# Parte 3
+print("Parte 3: Al final, el dragón resultó ser su mascota perdida.") 
